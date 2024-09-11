1d36eff50e0850336e4b9a471ad5ff62e3ee6d1e
Tuychiev Otabek <otabekotabek996996@gmail.com>
2024-09-11 15:03:43 +0500

    feat: add routers to pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,20 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Home from "./pages/home/Home";
+import SignInPage from "./pages/sign-in/SignInPage";
+import SignUpPage from "./pages/sign-up/SignUpPage";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>This is App</h1>
+      <Routes>
+        <Route path="/order-management-app-02" element={<Home />} />
+        <Route path="/order-management-app-02/sign-in" element={<SignInPage />} />
+        <Route path="/order-management-app-02/sign-up" element={<SignUpPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
