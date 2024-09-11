363ecf5c861acfb3cc7c50e02a527acd4cdd87fb
Tuychiev Otabek <otabekotabek996996@gmail.com>
2024-09-11 15:05:21 +0500

    fix: rename home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Home from "./pages/home/Home";
+import HomePage from "./pages/home/HomePage";
 import SignInPage from "./pages/sign-in/SignInPage";
 import SignUpPage from "./pages/sign-up/SignUpPage";
 
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <h1>This is App</h1>
       <Routes>
-        <Route path="/order-management-app-02" element={<Home />} />
+        <Route path="/order-management-app-02" element={<HomePage />} />
         <Route path="/order-management-app-02/sign-in" element={<SignInPage />} />
         <Route path="/order-management-app-02/sign-up" element={<SignUpPage />} />
       </Routes>
