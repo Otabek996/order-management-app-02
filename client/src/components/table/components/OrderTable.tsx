c39e5f448d49b571a4d82d81ce120ee70f8799f0
Tuychiev Otabek <otabekotabek996996@gmail.com>
2024-09-12 12:36:20 +0500

    feat: add table pagination

--- client/src/components/table/components/OrderTable.tsx
+++ client/src/components/table/components/OrderTable.tsx
@@ -25,12 +25,21 @@ import Menu from "@mui/joy/Menu";
 import MenuButton from "@mui/joy/MenuButton";
 import MenuItem from "@mui/joy/MenuItem";
 import Dropdown from "@mui/joy/Dropdown";
+import { styled } from "@mui/system";
+import {
+  TablePagination,
+  tablePaginationClasses as classes,
+} from "@mui/base/TablePagination";
+import FirstPageRoundedIcon from "@mui/icons-material/FirstPageRounded";
+import LastPageRoundedIcon from "@mui/icons-material/LastPageRounded";
+import ChevronLeftRoundedIcon from "@mui/icons-material/ChevronLeftRounded";
+import ChevronRightRoundedIcon from "@mui/icons-material/ChevronRightRounded";
 
 import FilterAltIcon from "@mui/icons-material/FilterAlt";
 import SearchIcon from "@mui/icons-material/Search";
 import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import CheckRoundedIcon from "@mui/icons-material/CheckRounded";
-import HourglassTopIcon from '@mui/icons-material/HourglassTop';
+import HourglassTopIcon from "@mui/icons-material/HourglassTop";
 import AutorenewRoundedIcon from "@mui/icons-material/AutorenewRounded";
 import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
 import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
@@ -43,37 +52,23 @@ import {
   useDeleteOrderMutation,
 } from "../../../store/orders.api";
 
+interface DataType {
+  id: string;
+  name: string;
+  email: string;
+  status: string;
+  date: string;
+}
+
 type DataOrderType = {
   id: string | number;
-  username?: string;
+  name?: string;
+  email?: string;
   status?: string;
-  createdAt?: string;
+  date?: string;
 };
 
-type StatusType = 'Delivered' | 'Sent' | 'Pending';
-
-// const data = [
-//   {
-//     id: "INV-1217",
-//     date: "Feb 3, 2023",
-//     status: "Paid",
-//     customer: {
-//       initial: "S",
-//       name: "Sachin Flynn",
-//       email: "s.flyn@email.com",
-//     },
-//   },
-//   {
-//     id: "INV-1216",
-//     date: "Feb 3, 2023",
-//     status: "Cancelled",
-//     customer: {
-//       initial: "B",
-//       name: "Bradley Rosales",
-//       email: "brad123@email.com",
-//     },
-//   },
-// ];
+type StatusType = "Delivered" | "Sent" | "Pending";
 
 function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
   if (b[orderBy] < a[orderBy]) {
@@ -154,6 +149,9 @@ export default function OrderTable() {
     </React.Fragment>
   );
 
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
   const [count, setCount] = useState("");
 
   const { data = [], isLoading } = useGetOrdersQuery(count);
@@ -163,7 +161,24 @@ export default function OrderTable() {
 
   const formatDate = (dateString: string): string => {
     const date = new Date(dateString);
-    return date.toISOString().split('T')[0]; // Extract the YYYY-MM-DD part
+    return date.toISOString().split("T")[0]; // Extract the YYYY-MM-DD part
+  };
+
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - data.length) : 0;
+
+  const handleChangePage = (
+    event: React.MouseEvent<HTMLButtonElement> | null,
+    newPage: number
+  ) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
   };
 
   return (
@@ -186,7 +201,7 @@ export default function OrderTable() {
         >
           <FilterAltIcon />
         </IconButton>
-        
+
         <Modal open={open} onClose={() => setOpen(false)}>
           <ModalDialog aria-labelledby="filter-modal" layout="fullscreen">
             <ModalClose />
@@ -265,7 +280,9 @@ export default function OrderTable() {
                   checked={selected.length === data.length}
                   onChange={(event) => {
                     setSelected(
-                      event.target.checked ? data.map((element: DataOrderType) => element.id) : []
+                      event.target.checked
+                        ? data.map((element: DataOrderType) => element.id)
+                        : []
                     );
                   }}
                   color={
@@ -307,13 +324,20 @@ export default function OrderTable() {
             </tr>
           </thead>
           <tbody>
-            {[...data].sort(getComparator(order, "id")).map((element) => (
+            {(rowsPerPage > 0
+              ? [...data]
+                  .sort(getComparator(order, "id"))
+                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              : [...data].sort(getComparator(order, "id"))
+            ).map((element: DataType) => (
               <tr key={element.id}>
                 <td style={{ textAlign: "center", width: 120 }}>
                   <Checkbox
                     size="sm"
                     checked={selected.includes(element.id)}
-                    color={selected.includes(element.id) ? "primary" : undefined}
+                    color={
+                      selected.includes(element.id) ? "primary" : undefined
+                    }
                     onChange={(event) => {
                       setSelected((ids) =>
                         event.target.checked
@@ -329,7 +353,9 @@ export default function OrderTable() {
                   <Typography level="body-xs">{element.id}</Typography>
                 </td>
                 <td>
-                  <Typography level="body-xs">{formatDate(element.date)}</Typography>
+                  <Typography level="body-xs">
+                    {formatDate(element.date)}
+                  </Typography>
                 </td>
                 <td>
                   <Chip
@@ -357,12 +383,8 @@ export default function OrderTable() {
                   <Box sx={{ display: "flex", gap: 2, alignItems: "center" }}>
                     <Avatar size="sm">{element.name.charAt(0)}</Avatar>
                     <div>
-                      <Typography level="body-xs">
-                        {element.name}
-                      </Typography>
-                      <Typography level="body-xs">
-                        {element.email}
-                      </Typography>
+                      <Typography level="body-xs">{element.name}</Typography>
+                      <Typography level="body-xs">{element.email}</Typography>
                     </div>
                   </Box>
                 </td>
@@ -373,10 +395,46 @@ export default function OrderTable() {
                 </td>
               </tr>
             ))}
+
+            {emptyRows > 0 && (
+              <tr style={{ height: 41 * emptyRows }}>
+                <td colSpan={6} aria-hidden />
+              </tr>
+            )}
           </tbody>
+          <tfoot>
+            <tr>
+              <CustomTablePagination
+                className="Pagination-laptopUp"
+                rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
+                colSpan={6}
+                count={13}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                slotProps={{
+                  select: {
+                    "aria-label": "Rows per page",
+                  },
+                  actions: {
+                    showFirstButton: true,
+                    showLastButton: true,
+                    slots: {
+                      firstPageIcon: FirstPageRoundedIcon,
+                      lastPageIcon: LastPageRoundedIcon,
+                      nextPageIcon: ChevronRightRoundedIcon,
+                      backPageIcon: ChevronLeftRoundedIcon,
+                    },
+                  },
+                }}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+              />
+            </tr>
+          </tfoot>
         </Table>
       </Sheet>
-      <Box
+
+      {/* <Box
         className="Pagination-laptopUp"
         sx={{
           pt: 2,
@@ -409,7 +467,7 @@ export default function OrderTable() {
           </IconButton>
         ))}
         <Box sx={{ flex: 1 }} />
-        
+
         <Button
           size="sm"
           variant="outlined"
@@ -418,7 +476,125 @@ export default function OrderTable() {
         >
           Next
         </Button>
-      </Box>
+      </Box> */}
     </React.Fragment>
   );
 }
+
+const blue = {
+  200: "#A5D8FF",
+  400: "#3399FF",
+};
+
+const grey = {
+  50: "#F3F6F9",
+  100: "#E5EAF2",
+  200: "#DAE2ED",
+  300: "#C7D0DD",
+  400: "#B0B8C4",
+  500: "#9DA8B7",
+  600: "#6B7A90",
+  700: "#434D5B",
+  800: "#303740",
+  900: "#1C2025",
+};
+
+const CustomTablePagination = styled(TablePagination)(
+  ({ theme }) => `
+  & .${classes.spacer} {
+    display: none;
+  }
+
+  & .${classes.toolbar}  {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 10px;
+    
+
+    @media (min-width: 768px) {
+      flex-direction: row;
+      align-items: center;
+    }
+  }
+
+  & .${classes.selectLabel} {
+    margin: 0;
+  }
+
+  & .${classes.select}{
+    font-family: 'IBM Plex Sans', sans-serif;
+    padding: 2px 0 2px 4px;
+    border: 1px solid ${theme.palette.mode === "dark" ? grey[800] : grey[200]};
+    border-radius: 6px; 
+    background-color: transparent;
+    color: ${theme.palette.mode === "dark" ? grey[300] : grey[900]};
+    transition: all 100ms ease;
+
+    &:hover {
+      background-color: ${theme.palette.mode === "dark" ? grey[800] : grey[50]};
+      border-color: ${theme.palette.mode === "dark" ? grey[600] : grey[300]};
+    }
+
+    &:focus {
+      outline: 3px solid ${
+        theme.palette.mode === "dark" ? blue[400] : blue[200]
+      };
+      border-color: ${blue[400]};
+    }
+  }
+
+  & .${classes.displayedRows} {
+    margin: 0;
+
+    @media (min-width: 768px) {
+      margin-left: auto;
+    }
+  }
+
+  & .${classes.actions} {
+    display: flex;
+    gap: 6px;
+    border: transparent;
+    text-align: center;
+  }
+
+  & .${classes.actions} > button {
+    display: flex;
+    align-items: center;
+    padding: 0;
+    border: transparent;
+    border-radius: 50%; 
+    background-color: transparent;
+    border: 1px solid ${theme.palette.mode === "dark" ? grey[800] : grey[200]};
+    color: ${theme.palette.mode === "dark" ? grey[300] : grey[900]};
+    transition: all 100ms ease;
+
+    > svg {
+      font-size: 22px;
+    }
+
+    &:hover {
+      background-color: ${theme.palette.mode === "dark" ? grey[800] : grey[50]};
+      border-color: ${theme.palette.mode === "dark" ? grey[600] : grey[300]};
+    }
+
+    &:focus {
+      outline: 3px solid ${
+        theme.palette.mode === "dark" ? blue[400] : blue[200]
+      };
+      border-color: ${blue[400]};
+    }
+
+    &:disabled {
+      opacity: 0.3;
+      &:hover {
+        border: 1px solid ${
+          theme.palette.mode === "dark" ? grey[800] : grey[200]
+        };
+        background-color: transparent;
+      }
+    }
+  }
+  `
+);
