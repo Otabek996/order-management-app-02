142ce845c1f09ed66cc9cad12b1ee8c93fc05d4f
Tuychiev Otabek <otabekotabek996996@gmail.com>
2024-09-12 21:39:04 +0500

    fix: fix pagination count

--- client/src/components/table/components/OrderTable.tsx
+++ client/src/components/table/components/OrderTable.tsx
@@ -398,7 +398,7 @@ export default function OrderTable() {
                 className="Pagination-laptopUp"
                 rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
                 colSpan={6}
-                count={13}
+                count={data.length}
                 rowsPerPage={rowsPerPage}
                 page={page}
                 slotProps={{
